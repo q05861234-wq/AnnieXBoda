c77a9fe77fe9323032eb70b07f86fe267be525b8
q05861234-wq <q05861234@gmail.com>
2026-01-19 22:13:12 +0200

    Update Youtube.py

--- AnnieXMedia/platforms/Youtube.py
+++ AnnieXMedia/platforms/Youtube.py
@@ -14,7 +14,7 @@ from concurrent.futures import ThreadPoolExecutor
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch, Playlist
+from youtubesearchpython.aio import VideosSearch, Playlist
 
 # --- محاولة استيراد الإعدادات من البوت ---
 try:
